9dec64d3c2ce031299fa544627f2f8d63c5ffaad
Cupcake <cupcake@Stevens-MacBook-Air.local>
2024-02-03 22:51:15 +0100

    df

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import streamlit as st
 import pandas as pd
 from dotenv import load_dotenv
-from pandasai import SmartDataframe
+# from pandasai import SmartDataframe
+from pandasai import PandasAI
 from pandasai.llm.openai import OpenAI
 import os
+
+load_dotenv()
+
+my_token = os.getenv("OPENAI_API_KEY")
+
+llm = OpenAI(my_token)
 
 st.title("Readify")
 
 uploaded_file = st.file_uploader("Upload a CSV file to analyse")
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file) #create dataframe
+    # pandas_ai = SmartDataframe(df, config={"llm": llm})
+    pandas_ai = PandasAI(llm)
     st.write(df.head(10)) #display the first 3 rows , wrritten to our UI
     
     prompt = st.text_area("yout prompt : ")
     if st.button("Generate"):
         if prompt:
-                st.write("Generating response ...")
+            with st.spinner("Generating response ..."):
+                st.write(pandas_ai.run(df, prompt))
         else:
             st.warning("Please enter a Prompt. ")
             
-    load_dotenv()
-
-    my_token = os.getenv("OPENAI_API_KEY")
-
-    llm = OpenAI(my_token)
-    pandas_ai = SmartDataframe(df, config={"llm": llm})
-
